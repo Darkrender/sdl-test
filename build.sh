a672220aa703121f03738bfc7ff15b6a37b1e118
Darkrender <jimmiegisclair@gmail.com>
2022-04-08 14:32:57 -0500

    Add comments to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+# We want to remove the build directory if it exists,
+# then re-create it, move into it, and rebuild and compile with cmake
+
 rm -r build
 [ -d build ] || { mkdir build; [ -d build ] || exit 1; }
 cd build || exit 1
