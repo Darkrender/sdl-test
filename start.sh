9a0c84f691fb82f257a285617ce69e58925ba37b
Darkrender <jimmiegisclair@gmail.com>
2022-04-08 14:33:25 -0500

    Refactor start.sh to be usable on windows
    Also add comments

--- start.sh
+++ start.sh
@@ -1,3 +1,22 @@
-[ -d build ] && cmake --build ./build
+#!/bin/sh
+
+# If build directory doesn't exist, run the build.sh script which will create it and run some cmake commands
 [ ! -d build ] && echo "Directory /build/ DOES NOT exists. Running CMake..." && ./build.sh
-./build/SDLTest
+
+# If the build folder does exist, navigate into it and recompile with cmake
+[ -d build ] && cd build && cmake --build .
+
+# The final executable file will be in a different location depending on platform
+# On mac, it will just end up in the root of the build folder...
+if [ -e SDLTest ]
+then
+    ./SDLTest
+
+# On windows, it will typically end up in build/Debug/<ProjectName>.exe
+# unless we specifically make a release build
+elif [ -e Debug/SDLTest.exe ]
+then
+    ./Debug/SDLTest.exe
+else
+    echo "Couldn't find executable to run. Check the build folder for the path to the executable..."
+fi
